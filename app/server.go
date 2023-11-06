4828c8de375a5072d381c0931935bc507753d017
Lucho00Cuba <lomv0209@gmail.com>
2023-11-06 17:16:47 +0100

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -6,20 +6,40 @@ import (
 	"os"
 )
 
+const PORT = 4221
+const ADDR = "0.0.0.0"
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listen_addr := fmt.Sprintf("%s:%d", ADDR, PORT)
+
+	l, err := net.Listen("tcp", listen_addr)
+	defer l.Close()
+
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		fmt.Println("Failed to bind to port ", PORT)
 		os.Exit(1)
 	}
 
-	fmt.Println("Listening in 0.0.0.0:4221")
+	fmt.Println("Listening in ", listen_addr)
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	_, err = conn.Read(make([]byte, 1024))
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+	}
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error writing: ", err.Error())
+	}
+
+	defer conn.Close()
+
 }
